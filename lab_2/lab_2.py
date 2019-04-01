ce3c772b71e1c38e2e29c4058675c07b45a17529
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-04-01 18:53:22 -0500

    Lab2: improvements to basic ES

--- lab_2/lab_2.py
+++ lab_2/lab_2.py
@@ -1,12 +1,14 @@
 from numpy import random
 
 # 1.
-def evo_strategy(opt_f, size, max_gen, variance,k, c):
-    x = random.uniform(-2.048, 2.048, size)
+
+
+def evo_strategy(opt_f, size, limits, max_gen, sigma, k, c):
+    x = random.uniform(limits[0], limits[1], size)
     fx = opt_f(x)
     p = 0
     for i in range(max_gen):
-        x += random.normal(scale=variance)
+        x = mutate(x, 0, sigma, limits)
         new_fx = opt_f(x)
 
         if new_fx > fx:
@@ -18,9 +20,20 @@ def evo_strategy(opt_f, size, max_gen, variance,k, c):
         if i % k == 0:
             tmp = p/k
             if tmp < 0.2:
-                variance /= c
+                sigma /= c
             elif tmp > 0.2:
-                variance *= c
+                sigma *= c
+
+
+def mutate(x, mu, sigma, limits):
+    rnd = random.normal(loc=mu, scale=sigma, size=x.size)
+    new_x = x + rnd
+    while not (all(limits[0] <= i < limits[1] for i in new_x)):
+        rnd = random.normal(loc=mu, scale=sigma, size=x.size)
+        new_x = x + rnd
+
+    return new_x
+
 
 # (μ + 1)-EE, combinación lineal convexa
 
@@ -31,4 +44,5 @@ def func_1(x):
 
 
 if __name__ == '__main__':
-    evo_strategy(func_1, 2, 40, 3, 10, 0.817)
+    evo_strategy(opt_f=func_1, size=2, limits=(-2.048, 2.048),
+                 max_gen=400, sigma=3, k=10, c=0.817)
