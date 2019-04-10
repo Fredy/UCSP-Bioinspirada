313a1aa8437b7e2c96bd0640fdc51b8ab3b58a6b
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-04-10 16:51:06 -0500

    Lab3: fixing representation

--- lab_3/lab_3.py
+++ lab_3/lab_3.py
@@ -9,6 +9,39 @@ from random import choices, randrange, random
 # Representation
 
 
+class Cromosome:
+    """Represents each of the population individuals"""
+
+    def __init__(self, limits, precisions):
+        if len(limits) != len(precisions):
+            raise ValueError(
+                'len(limits) must equals len(precisions): {} != {}'.format(
+                    len(limits), len(precisions)
+                )
+            )
+
+        self.bin_real_repr = [
+            BinRealRepr(lim[0], lim[1], prec)
+            for lim, prec in zip(limits, precisions)
+        ]
+        self.bin_value = []
+        self._acc_limits = [0]
+
+        for idx, rep in enumerate(self.bin_real_repr):
+            self.bin_value.extend(rep.randomize())
+            last = self._acc_limits[-1]
+            self._acc_limits.append(last + rep.length)
+
+    def get(self, index):
+        if index >= len(self.bin_real_repr):
+            raise IndexError('Max index is {}'.format(len(self.bin_real_repr) - 1))
+
+        start = self._acc_limits[index]
+        end = self._acc_limits[index + 1]
+        return self.bin_value[start:end]
+
+    
+
 class BinRealRepr:
     """Binary to Real representation"""
 
@@ -19,11 +52,11 @@ class BinRealRepr:
         self.length = round(log2(right - left) + precision * log2(10))
 
     def randomize(self):
-        self.bin_value = choices(['0', '1'], k=self.length)
+        return choices([False, True], k=self.length)
 
-    def get_real_val(self):
+    def get_real_val(self, bin_value):
         acc = 0
-        for i, v in enumerate(self.bin_value[::-1]):
+        for i, v in enumerate(bin_value[::-1]):
             acc += 2**i * int(v)
         acc *= (self.right - self.left) / (2 ** self.length - 1)
         return round(self.left + acc, self.precision)
@@ -85,7 +118,8 @@ def linear_normalization(fitnesses, minv, maxv):
 
 # Operators
 
-def crossover1(gen1,gen2, length, point=None):
+
+def crossover1(gen1, gen2, length, point=None):
     if point is None:
         point = randrange(0, length)
     return (
@@ -93,25 +127,27 @@ def crossover1(gen1,gen2, length, point=None):
         gen2[:point] + gen1[point:]
     )
 
-def crossover2(gen1,gen2, length, point1=None, point2=None):
+
+def crossover2(gen1, gen2, length, point1=None, point2=None):
     if point1 is None:
         point1 = randrange(0, length)
     if point2 is None:
         point2 = randrange(0, length)
 
     if point1 > point2:
-        point1, point2 = point2, point1 
-    
+        point1, point2 = point2, point1
+
     return (
-    gen1[:point1] + gen2[point1:point2] + gen1[point2:],
-    gen2[:point1] + gen1[point1:point2] + gen2[point2:]
+        gen1[:point1] + gen2[point1:point2] + gen1[point2:],
+        gen2[:point1] + gen1[point1:point2] + gen2[point2:]
     )
 
-def crossover_uniform(gen1,gen2, length):
+
+def crossover_uniform(gen1, gen2, length):
     new_gen1 = []
     new_gen2 = []
     for i in range(length):
-        if randon() >= 0.5:
+        if random() >= 0.5:
             new_gen1.append(gen2[i])
             new_gen2.append(gen1[i])
         else:
