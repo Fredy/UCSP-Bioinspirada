8f9b92d742f6e0957930dad6ed10c8ab3a7b230b
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-06-18 00:08:57 -0500

    Lab 6: finished q learning

--- lab_6/lab_6.py
+++ lab_6/lab_6.py
@@ -1,21 +1,21 @@
+import sys
+import itertools
 import numpy as np
+from collections import defaultdict
 
-ALPHA = 0.5
-GAMMA = 0.5
+ALPHA = 0.5  # Learning rate
+GAMMA = 0.5  # Discount Factor
 
-ACTIONS = 4
-MAX_STATES = 1000  # Max iterations
+ACTIONS = 4  # up, down, left, right
+MAX_ITERATIONS = 1000  # Max iterations
 
 GRID_START = (0, 0)
-GRID_END = (4, 4)
-
+GRID_END = (100, 100)
 R_START, C_START = GRID_START
 R_END, C_END = GRID_END
 
-INITIAL_POS = (GRID_END[0]//2, GRID_END[1]//2)
+INITIAL_POS = (R_END//2, C_END//2)
 REWARD_POS = GRID_END
-
-initial_q = 0
 
 
 class Position:
@@ -31,7 +31,7 @@ class Position:
 
     def down(self):
         if self.r == R_END:
-            r = R_START
+            self.r = R_START
         else:
             self.r += 1
 
@@ -56,6 +56,17 @@ class Position:
             raise Exception(
                 f'Cannot compare {type(self)} with {type(other)} objects')
 
+    def move(self, i):
+        """0: up, 1: down, 2: left, 3: right"""
+        if i == 0:
+            self.up()
+        elif i == 1:
+            self.down()
+        elif i == 2:
+            self.left()
+        else:
+            self.right()
+
     def get_tuple(self):
         return (self.r, self.c)
 
@@ -76,4 +87,52 @@ def update(q):
     pass
 
 
-pos = (4, 4)
+def get_max_action(actions):
+    """
+    Return the index of the max value in actions.
+    If there are multiple max, return a random index.
+
+    :param actions: List of actions weights
+    """
+    maxs = np.argwhere(actions == actions.max()).flatten()
+
+    return np.random.choice(maxs)
+
+
+def q_learning():
+    q = defaultdict(lambda: np.ones(ACTIONS) / ACTIONS)
+
+    position = Position(*INITIAL_POS)
+
+    for t in itertools.count():
+        done = False
+        for i in range(MAX_ITERATIONS):
+            state = position.get_tuple()
+            action = get_max_action(q[state])
+
+            position.move(action)
+            next_state = position.get_tuple()
+
+            reward = get_reward(next_state)
+            tmp = reward + GAMMA * q[next_state].max()
+            tmp = tmp - q[state][action]
+            q[state][action] += ALPHA * tmp
+            
+            sys.stdout.write(f'\rT: {t} - {i}/{MAX_ITERATIONS}: Position: {next_state}')
+            sys.stdout.flush()
+
+
+            if reward == 1:
+                done = True
+                break
+
+        if done:
+            break
+        else:
+            position = Position(*INITIAL_POS)
+
+    return q
+
+if __name__ == "__main__":
+    q = q_learning()
+    # print(q)
